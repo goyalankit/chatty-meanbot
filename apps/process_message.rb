6d573344c452223341c6a00605fec8dc45adb72d
ankitunique <ankitunique@yahoo.com>
2013-01-27 00:13:34 +0530

    code improvement, bitly url sanitisation and commit message feature

--- apps/process_message.rb
+++ apps/process_message.rb
@@ -17,7 +17,7 @@ class ProcessMessage
     return "Format: last_tweet <screen name> Result: get last tweet of given user" if screen_name.blank?
     response = Faraday.get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{screen_name}&count=1&trim_user=true")
     result   = JSON.parse(response.body)
-    response.status == 200 and !result.blank? ? result.first["text"] : "most probably user doesn't or hasn't tweeted yet."
+    (response.status == 200 and !result.blank?) ? result.first["text"] : "most probably user doesn't exist or hasn't tweeted yet."
   end
 
   def ping test
@@ -46,27 +46,24 @@ class ProcessMessage
     "http://lmgtfy.com/?q=#{keyword}"
   end
 
+  def commit_message dummy
+    response = Faraday.get('http://whatthecommit.com/index.txt')
+    response.status == 200 ? response.body : "something went wrong.."
+  end
+
   def shorten long_url
     return "Format: shorten <url> Result: get bit.ly url for your long url" if long_url.blank?
-    encoded_long_url = URI::encode(long_url)
-    response = Faraday.get("https://api-ssl.bitly.com/v3/shorten?access_token=#{BITLY_AUTH_TOKEN}&longUrl=#{encoded_long_url}")
-    result   = response.body.blank? ? nil : JSON.parse(response.body)
-    if result.nil? or result['status_code'] == 500 or response.status != 200
-      logger("url shortened for user", "error")
-      "something went wrong...please recheck your url"
-    else
-      logger("url shortened for user")
-      JSON.parse(result)["data"]["url"]
-    end
+    encoded_long_url = URI::encode(long_url.strip.gsub(/\/*\?/,'/?'))
+    response = Faraday.get("https://api-ssl.bitly.com/v3/shorten?access_token=#{BITLY_AUTH_TOKEN}&longUrl=#{encoded_long_url}&format=txt")
+    response.status == 200 ? response.body : "invalid url. try again with some other url"
+  end
+
+  def about dummy
+    return "created by Ankit Goyal. @_goyalankit at Twitter. source code available at: https://github.com/ankitunique/chatty-meanbot"
   end
 
   def help args
-    return "Available commands are ping, define, use, shorten, last_tweet_for, let_me_google" if args.blank?
-    return "Format: ping \nResult: pong" if args.first == 'ping'
-    return "Format: define <word> \nResult: get definition of your word" if args.first == 'define'
-    return "Format: use <word> \nResult: get example sentences for your word" if args.first == 'use'
-    return "Format: shorten <url> \nResult: get bit.ly url for your long url" if args.first == 'shorten'
-    return "Format: last_tweet <screen name> \nResult: get last tweet of given user" if args.first == 'last_tweet_for'
-    return "Format: let_me_google <word> \nResult: get let me google it for you animation link" if args.first == 'let_me_google'
+    return send(args.first, nil) unless args.blank?
+    return "Available commands are ping, define, use, shorten, last_tweet_for, let_me_google, commit_message\n type help <command name to get info about a command>"
   end
 end
