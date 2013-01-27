6869a3ca93b06651e7b9b944d5b3b69cef94f484
ankitunique <ankitunique@yahoo.com>
2013-01-27 22:44:59 +0530

    simple server

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require 'bundler'
 Bundler.require
-
 require 'socket'
 
 Dir[File.join(".", "apps/*.rb")].each {|file| require file}
 
-ds = UDPSocket.new
-ds.bind(nil, $PORT || 5000)
+server = TCPServer.new($PORT || 5000)
+loop {
+  bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
+  puts "**** chatty side of mr Bot is live ****"
+  bot.status("@_goyalankit at twitter.com")
+  bot.listen_for_messages
+}
 
-bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
-puts "**** chatty side of mr Bot is live ****"
-bot.status("@_goyalankit at twitter.com")
-bot.listen_for_messages
