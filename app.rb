5313ce40d883f83021e7f3427e2a24405d6778d0
ankitunique <ankitunique@yahoo.com>
2013-01-27 22:09:37 +0530

    adding dummy UDP socket

--- app.rb
+++ app.rb
@@ -1,19 +1,14 @@
 require 'bundler'
 Bundler.require
 
+require 'socket'
+
 Dir[File.join(".", "apps/*.rb")].each {|file| require file}
 
-get '/start-chatty-bot' do
-  td1 = Thread.new do 
-    bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
-    puts "**** chatty side of mr Bot is live ****"
-    bot.status(params[:message] || "@_goyalankit at twitter.com")
-    bot.listen_for_messages
-  end
+ds = UDPSocket.new
+ds.bind(nil, ENV['PORT'])
 
-  body "process started at #{Time.now}"
-end
-
-get '/ping' do
-  "App running at #{Time.now}"
-end
+bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
+puts "**** chatty side of mr Bot is live ****"
+bot.status("@_goyalankit at twitter.com")
+bot.listen_for_messages
