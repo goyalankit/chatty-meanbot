959357453bb9c310db318eaccd5ecd8054767346
ankitunique <ankitunique@yahoo.com>
2013-01-27 13:16:20 +0530

    compatibility with ruby 1.9.2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
-gem 'xmpp4r-simple'
+gem "xmpp4r-simple", :git => 'https://github.com/blaine/xmpp4r-simple.git'
 gem 'wordnik'
 gem 'faraday'
 gem 'pry'
+gem 'foreman'
--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 
-Dir["apps/*.rb"].each {|file| require file }
+Dir[File.join(".", "**/*.rb")].each {|file| require file}
 
 bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
 puts "**** chatty side of mr Bot is live ****"
