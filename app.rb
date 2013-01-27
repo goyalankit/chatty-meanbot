d0da0e2a86adf908e3d9acc601a84137aaee7747
ankitunique <ankitunique@yahoo.com>
2013-01-27 22:16:56 +0530

    adding port variable

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'socket'
 Dir[File.join(".", "apps/*.rb")].each {|file| require file}
 
 ds = UDPSocket.new
-ds.bind(nil, ENV['PORT'])
+ds.bind(nil, $PORT || 5000)
 
 bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
 puts "**** chatty side of mr Bot is live ****"
