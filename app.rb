994e30ed09504937b2b800ebe58227c3a9c2779f
ankitunique <ankitunique@yahoo.com>
2013-01-27 15:52:41 +0530

    adding thread

--- app.rb
+++ app.rb
@@ -4,14 +4,13 @@ Bundler.require
 Dir[File.join(".", "apps/*.rb")].each {|file| require file}
 
 get '/start-chatty-bot' do
-  job1 = fork do
+  td1 = Thread.new do 
     bot = Bot.new(ENV['BOT_USERNAME'], ENV['BOT_PASSWORD'])
     puts "**** chatty side of mr Bot is live ****"
     bot.status(params[:message] || "@_goyalankit at twitter.com")
     bot.listen_for_messages
   end
 
-  Process.detach(job1)
   body "process started at #{Time.now}"
 end
 
